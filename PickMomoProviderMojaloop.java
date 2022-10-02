8ef0ab8818f64e09720fde2dc7f02663dbc72b40
Victor Joseph Mnyawami <44136836+Victor-M-Joseph@users.noreply.github.com>
2022-10-02 04:29:32 +0300

    Add files via upload

--- PickMomoProviderMojaloop.java
+++ PickMomoProviderMojaloop.java
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 public class PickMomoProviderMojaloop extends AppCompatActivity {
 
@@ -24,6 +39,7 @@ public class PickMomoProviderMojaloop extends AppCompatActivity {
     TextView mpesaNumber;
 
     String pickedMomo;
+    String mobileMoneyNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -106,13 +122,117 @@ public class PickMomoProviderMojaloop extends AppCompatActivity {
 
                 if (pickedMomo!=null) {
 
-                    Intent intent = new Intent(PickMomoProviderMojaloop.this, EnterOtpMojaloopActivity.class);
-                    startActivity(intent);
+                    mojaloopPostLinkingRequestConsent(pickedMomo, mobileMoneyNumber);
+
+
                 }
 
             }
         });
     }
+
+
+    public void mojaloopPostLinkingRequestConsent(String Provider, String MSIDSN){
+
+
+        OkHttpClient client1 = new OkHttpClient();
+
+        String url = "/linking/request-consent";
+
+
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\n" +
+                "  \"consentRequestId\": \"f6ab43b0-71cc-49f9-b763-2ac3f05ac8c1\",\n" +
+                "  \"toParticipantId\": \"dfspa\",\n" +
+                "  \"accounts\": [\n" +
+                "    {\n" +
+                "      \"accountNickname\": \"SpeXXXXXXXXnt\",\n" +
+                "      \"id\": \"dfspa.username.5678\",\n" +
+                "      \"currency\": \"USD\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"actions\": [\n" +
+                "    \"ACCOUNTS_TRANSFER\"\n" +
+                "  ],\n" +
+                "  \"userId\": \"username1234\",\n" +
+                "  \"callbackUri\": \"pisp-app://callback\"\n" +
+                "}");
+
+
+        Request request1 = new Request.Builder()
+                .url(url)
+                .method("POST", body)
+                .build();
+
+        client1.newCall(request1).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+
+                        //Notify user of failure
+
+                    }
+                });
+
+                e.printStackTrace();
+
+                Log.d("Make Deposit:", "getting azamToken Failed");
+
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Response response0) throws IOException {
+
+                String response01 = response0.body().string();
+
+                final JSONObject[] JSONresponse01 = new JSONObject[1];
+                try {
+                    JSONresponse01[0] = new JSONObject(response01);
+                    Boolean success = JSONresponse01[0].getBoolean("success");
+
+                    if (success.equals(true)) {
+
+                        //go to Linking
+
+                        Intent intent = new Intent(PickMomoProviderMojaloop.this, EnterOtpMojaloopActivity.class);
+                        startActivity(intent);
+
+
+                    }
+                    else {
+                        //Notice user of failure
+                    }
+
+
+                } catch (JSONException e) {
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+
+                            //Notice user of failure
+
+                        }
+                    });
+
+                    e.printStackTrace();
+                }
+
+
+            }
+
+        });
+
+
+    }
+
+
+
 
 
     private void  editAccountNumberMojaloopDialog(){
@@ -132,7 +252,7 @@ public class PickMomoProviderMojaloop extends AppCompatActivity {
 
                 String strNewMomoNumber = newMomoNumber.getText().toString().trim();
 
-                String mobileMoneyNumber = "+255"+strNewMomoNumber;
+                mobileMoneyNumber = "+255"+strNewMomoNumber;
 
                     //mobileMoneyOption.setTextColor(Color.parseColor("#21618C"));
                     mpesaNumber.setText(mobileMoneyNumber);
